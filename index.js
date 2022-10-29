b87ea54d6ab552fa7fc0b26c4b4d9c9e1870379c
Artem Dudkin <artem.dudkin@visitech.ru>
2022-10-29 17:15:10 +0300

    multiple tables

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /**
- * jq-table-sort 0.0.1
- * 
- * @requires jq-min (i.e. +2k)
+ * @requires jq-min (https://github.com/artemdudkin/jq-min)
+ *
  * @returns
+ *
  *   window.tableSort.initAll = reinitialize all, i.e. tables that have attributes "table-sort")
  *     (I use it after recreating all items, so it does not prevent initialization of element that was initialized before)
  *     (also, it runs on DOMContentLoaded so you do not need to call it if you are not changing elements)
+ *
+ *   window.tableSort.setReadDataFunc = <function(cell, table)> if you need preprocess data at table cells before comparing
+ *
  * @license MIT
  */
 (function(){
@@ -21,13 +24,45 @@
 
 
   function initAll() {
-    let trs = $('[table-sort] thead tr');
-    if (!trs[0]) trs = $('[table-sort] tr');
-    if (trs[0]) {
-      for (let i=0; i<trs[0].children.length; i++) {
-        init(trs[0].children[i], i);
+    let tables = $('[table-sort]');
+    for (let i=0; i<tables.length; i++) {
+      if (!tables[i].status) tables[i].status = {}
+      if (!tables[i].status.initialized) {
+        let trs = $('thead tr', tables[i]);
+        if (!trs[0]) {
+          trs = $('tr', tables[i]);
+          tables[i].status.exclude_first_row = true;
+        }
+        if (trs[0]) {
+          for (let j=0; j<trs[0].children.length; j++) {
+            init(trs[0].children[j], j);
+          }
+        }
+        tables[i].status.initialized = true;
       }
     }
+  }
+
+  function initTableData(tbl, forceReinit) {
+      if (!tbl.status) tbl.status = {}
+
+      if (!tbl.status.data || forceReinit) {
+        tbl.status.data = [];
+
+        let trs = $('tbody tr', tbl);
+        for (let i=0; i<trs.length; i++) {
+          trs[i].tableSortId = i;
+          let line = {id:i}
+          let tds = $('td', trs[i]);
+          for (let j=0; j<tds.length; j++) {
+            let value = rd(tds[j], tbl);
+            if (typeof value === 'undefined') value = tds[j].innerText
+            line[j] = value;
+          }
+        tbl.status.data.push(line);
+        }
+        if (tbl.status.exclude_first_row) tbl.status.data.shift();
+      }
   }
 
   function init(itm, index) {
@@ -50,20 +85,8 @@
           tbl.status.el = tbl.status.srt ? itm : undefined;
           itm.innerHTML = itm.innerHTML + (tbl.status.srt ? '<span class="table-sort-marker">&nbsp;'+(tbl.status.srt===1?'&#x25BE;':'&#x25B4;')+'</span>' : '');
 
-          //get table data fi needed
-          let trs = $('tbody tr', tbl);
-          if (!tbl.status.data) {
-            tbl.status.data = [];
-            for (let i=0; i<trs.length; i++) {
-              trs[i].tableSortId = i;
-              let line = {id:i}
-              let tds = $('td', trs[i]);
-              for (let j=0; j<tds.length; j++) {
-                line[j] = tds[j].innerText;
-              }
-              tbl.status.data.push(line);
-            }
-          }
+          //get table data
+          initTableData(tbl);
 
           //sort table data
           if (tbl.status.srt) {
@@ -89,21 +112,31 @@
 
           //place table rows in accordance with order of table data rows
           let prev;
+          let trs = $('tbody tr', tbl);
           for (let i=0; i<tbl.status.data.length-1; i++) {
             let id = tbl.status.data[i].id;
-            let el; for (let i=0; i<trs.length; i++) {
-              if (trs[i].tableSortId === id) el=trs[i];
-            }
+            let el; for (let i=0; i<trs.length; i++) if (trs[i].tableSortId === id) el=trs[i];
             if (prev) {
               prev.after(el)
             } else {
-              $('tbody', tbl)[0].prepend(el);
+              if (tbl.status.exclude_first_row) {
+                let header; for (let i=0; i<trs.length; i++) if (trs[i].tableSortId === 0) header=trs[i];
+                header.after(el);
+              } else {
+                $('tbody', tbl)[0].prepend(el);
+              }
             }
             prev = el;
           }
 
           return false;
         });
+  }
+
+
+  let rd;
+  function setReadDataFunc( fn) { // fn = function(itm, tbl)
+    rd = fn;
   }
 
 
@@ -116,6 +149,7 @@
   }
 
   window.tableSort = {
-//    initAll,
+    initAll,
+    setReadDataFunc,
   }
 })();
